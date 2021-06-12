ee4c2ce64d2db0d831f41e269686da00a65a7c0f
Niraj-123456 <lamaniraj321@gmail.com>
2021-06-12 19:55:30 +0545

    message icon added

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,8 @@ function Header() {
                     <p>Social Media App</p>
                     </a>
                     <div className="d-flex">
-                        <i className="far fa-bell"></i>
+                        <i className="far fa-bell mx-3 h5"></i>
+                        <i class="fab fa-facebook-messenger h5"><span class="badge rounded-pill bg-primary">1</span></i>
                     </div>
                 </div>
             </nav>

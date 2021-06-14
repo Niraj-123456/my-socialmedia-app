3f2147b0082a665eecb07d38766e24f92979e8c8
Niraj-123456 <lamaniraj321@gmail.com>
2021-06-14 09:11:18 +0545

    comment section, buttons added

--- src/components/posts/ViewPost.js
+++ src/components/posts/ViewPost.js
@@ -2,13 +2,35 @@ import React from 'react'
 
 function ViewPost() {
     return (
-        <div className="viewpost-wrapper">
-            <div className="main-viewpost d-flex align-items-center">
-                <i className="fas fa-user-circle mx-2"></i>
-                <p>This is a users post...</p>
-            </div>
-            <div className="user-comments">
-                This is comment section
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 my-5 offset-2 shadow rounded">  
+                    <div className="row">
+                        <div className="col d-flex">
+                            <i className="fas fa-user-circle mx-2 p-3"></i>
+                            <p class="lead p-2">UserName</p>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-11 ms-5 px-2">
+                                <p className="fs-6">Adipisicing sunt irure Lorem proident labore sint. Labore nisi reprehenderit voluptate sunt mollit incididunt id sit dolore consectetur excepteur et culpa dolore. Sunt aliqua culpa minim eu non.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row gx-5">
+                        <div className="col-md-4 offset-8">
+                            <button className="btn btn-primary btn-sm me-1">Like <span>0</span></button>
+                            <button className="btn btn-primary btn-sm me-1">Comment <span>0</span></button>
+                            <button className="btn btn-primary btn-sm">Share</button>
+                        </div>
+                    </div>
+                    <hr className="w-2"/>
+                    <div className="row">
+                        <div className="col d-flex px-5">
+                            <i className="fas fa-user-circle me-3 p-1"></i>
+                            <p className="fs-6">This is comment section</p>
+                        </div>
+                    </div>
+                </div>  
             </div>
         </div>
     )
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD9YK9KVuNmN1q-HfE3eewFOy98PwxDh_A",
+    authDomain: "social-media-app-979b2.firebaseapp.com",
+    projectId: "social-media-app-979b2",
+    storageBucket: "social-media-app-979b2.appspot.com",
+    messagingSenderId: "453114933250",
+    appId: "1:453114933250:web:0acf134c69bcae378cb71b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);

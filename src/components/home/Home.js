3a7b8a8d23982857067e202dfe4c8077e76203a8
Niraj-123456 <lamaniraj321@gmail.com>
2021-07-03 17:41:32 +0545

    home page design changed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,13 +6,10 @@ function Home() {
   return (
     <div className="container-fluid">
       {/* <ViewPost /> */}
-      <div
-        className="row align-items-center"
-        style={{ height: "calc(100vh - 70px)" }}
-      >
-        <div className="col-md-6">
+      <div className="row">
+        <div className="col-md-6" style={styles}>
           <div
-            className="card m-auto"
+            className="card m-auto mt-5"
             style={{ width: "18rem", backgroundColor: "#92a8d1" }}
           >
             <div className="card-body">
@@ -28,10 +25,27 @@ function Home() {
               </Link>
             </div>
           </div>
+          <p
+            className="mt-5 mx-auto p-2 text-light fs-5"
+            style={{ width: "30rem" }}
+          >
+            Labore ut dolore id ad id sunt pariatur excepteur enim irure ea
+            reprehenderit elit quis. Cillum aliquip cillum officia ut veniam
+            cillum ea sint tempor aliqua aliqua dolore. Sit laborum voluptate
+            reprehenderit exercitation laboris laborum Lorem voluptate id. Et
+            irure deserunt ipsum officia laborum cupidatat consequat laboris
+            tempor. Magna dolore aute consequat aute ex esse eu velit eiusmod
+            amet minim.
+          </p>
         </div>
-        <div className="col-md-6">
+        <div className="col-md-6 mt-5">
+          <p className="m-auto p-2 fs-5" style={{ width: "30rem" }}>
+            Minim eiusmod esse consequat consequat anim est sunt cillum. Do
+            pariatur ut est eu qui ex irure labore ullamco. Esse duis culpa do
+            fugiat aliqua ut pariatur nostrud incididunt incididunt.
+          </p>
           <div
-            className="card m-auto"
+            className="card m-auto mt-5"
             style={{ width: "18rem", backgroundColor: "#92a8d1" }}
           >
             <div className="card-body">
@@ -50,3 +64,9 @@ function Home() {
 }
 
 export default Home;
+
+const styles = {
+  backgroundColor: "#6b5b95",
+  clipPath: "polygon(0 0, 100% 0, 83% 100%, 0% 100%)",
+  height: "calc(100vh - 70px)",
+};
